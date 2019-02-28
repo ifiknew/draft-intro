b7cc9d79d12d5a52072c1d566ec3c8f48b54b062
linzuhua <390162543@qq.com>
2019-02-28 23:34:22 +0800

    feat - transfer to rgb data with ratio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import CanvasDraw from "react-canvas-draw"
 import styles from './index.css';
 import { Icon } from 'antd'
 
+const EDGE = 300
+const MAX = 255
 class Index extends React.Component {
   state = {
     imageSrc: null,
@@ -11,10 +13,10 @@ class Index extends React.Component {
   canvas:any = null
 
   componentDidMount = () => {
-    window.setInterval(
-      this.getData,
-      1000
-    )
+    // window.setInterval(
+    //   this.getData,
+    //   1000
+    // )
   }
   handleDelete = () => {
     this.canvas.clear()
@@ -36,7 +38,7 @@ class Index extends React.Component {
         const image = new Image()
         image.onload = () => {
           this.handleDelete()
-          this.canvas.ctx.drawing.drawImage(image, 0, 0, 300, 300)
+          this.canvas.ctx.drawing.drawImage(image, 0, 0, EDGE, EDGE)
         }
         image.src = (e.target as any).result
       }
@@ -54,17 +56,25 @@ class Index extends React.Component {
   }
 
   getData = () => {
-    const data: ImageData = this.canvas.ctx.drawing.getImageData(0,0, 300, 300)
-    console.log(data)
+    if (this.canvas == null) { return }
+    const data: ImageData = this.canvas.ctx.drawing.getImageData(0,0, EDGE, EDGE)
+    const matrix = Array(EDGE).fill(Array(EDGE).fill([0, 0, 0]))
+    matrix.forEach((row, rowIndex) => {
+      row.forEach((pixel: Array<number>, columnIndex: number) => {
+        const beginIndex = (rowIndex * EDGE + columnIndex) * 4
+        row[columnIndex] = pixel.map((_, index) => Number.prototype.toFixed.call(data.data[beginIndex + index] / MAX, 2))
+      })
+    })
+    console.log(matrix)
   }
   render = () => {
     return (
       <div className={styles.body}>
         <CanvasDraw
-          canvasWidth={300}
-          canvasHeight={300}
+          canvasWidth={EDGE}
+          canvasHeight={EDGE}
           brushRadius={1}
-          lazyRadius={10}
+          lazyRadius={1}
           brushColor="#000"
           ref={this.getRef}
         />
@@ -72,6 +82,7 @@ class Index extends React.Component {
           <Icon type="delete" onClick={this.handleDelete} />
           <Icon type="undo" onClick={this.handleUndo} />
           <Icon type="file" onClick={this.handleUpload}/>
+          <Icon type="search" onClick={this.getData} />
         </div>
       </div>
     )
