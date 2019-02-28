c97f4ee239fc6c19956c1838a6d748d629163f6c
linzuhua <390162543@qq.com>
2019-02-28 20:15:32 +0800

    feat - base done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,80 @@
 import React from 'react';
+import CanvasDraw from "react-canvas-draw"
 import styles from './index.css';
+import { Icon } from 'antd'
 
+class Index extends React.Component {
+  state = {
+    imageSrc: null,
+    key: new Date().valueOf()
+  }
+  canvas:any = null
 
-export default function() {
-  return (
-    <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            Getting Started
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  componentDidMount = () => {
+    window.setInterval(
+      this.getData,
+      1000
+    )
+  }
+  handleDelete = () => {
+    this.canvas.clear()
+  }
+
+  handleUndo = () => {
+    this.canvas.undo()
+  }
+
+  handleUpload = () => {
+    
+    const input = document.createElement("input")
+    input.style.visibility = 'hidden'
+    input.type = 'file'
+    input.onchange = () => {
+      const file = input.files[0]
+      const fr = new FileReader()
+      fr.onload = (e) => {
+        const image = new Image()
+        image.onload = () => {
+          this.handleDelete()
+          this.canvas.ctx.drawing.drawImage(image, 0, 0, image.width, image.height)
+        }
+        image.src = (e.target as any).result
+      }
+      fr.readAsDataURL(file)
+      document.body.removeChild(input)
+      input.onchange = null
+    }
+    document.body.appendChild(input)
+    input.click()
+  }
+
+  getRef = (canvas) => {
+    window.c = canvas
+    this.canvas = canvas
+  }
+
+  getData = () => {
+    const data: ImageData = this.canvas.ctx.drawing.getImageData(0,0, window.innerWidth, window.innerHeight - 80)
+    console.log(data)
+  }
+  render = () => {
+    return (
+      <div key={this.state.key}>
+        <CanvasDraw
+          canvasWidth={window.innerWidth}
+          canvasHeight={window.innerHeight - 80}
+          brushRadius={2}
+          brushColor="#000"
+          ref={this.getRef}
+        />
+        <div className={styles.itemGroup}>
+          <Icon type="delete" onClick={this.handleDelete} />
+          <Icon type="undo" onClick={this.handleUndo} />
+          <Icon type="file" onClick={this.handleUpload}/>
+        </div>
+      </div>
+    )
+  }
 }
+
+export default Index
